9cc9109fc94788843d51a4196cc83c1f2241feb3
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2024-03-21 12:20:46 +0100

    finish with the js

--- text-to-speech-app/script.js
+++ text-to-speech-app/script.js
@@ -2,33 +2,54 @@ const resultElement = document.getElementById("result");
 let recognition;
 
 function startConverting(){
-    if('webkitSpeechRecognition' in window){    
+     if('webkitSpeechRecognition' in window){    
         recognition = new webkitSpeechRecognition();
         setupRecognition(recognition);
         recognition.start();
     }
 
 
-}
+ }
 
 function setupRecognition(recognition){
     recognition.continuos = true; // to continous recognition of the speaker
-    recognition.interimResults = true; // setup the recognition
+    recognition.interimResults = true; // setup the recognition listen to the speech of the user
     recognition.lang = 'en-US';
     recognition.onresult = function(event){
         //process result function will be call here 
-        processResult(event.results);
+
+        const{ finalTranscript , interTranscript} =  processResult(event.results);
+
+        resultElement.innerHTML = finalTranscript + interTranscript;
+       
     }
 
 }
 
-function processResult(result){
+function processResult(results){
+    let finalTranscript = " ";
+    let interTranscript = " ";
 
+    for (let i = 0; i < results.length; i++) {
+        let transcript = results[i][0].transcript;
+        transcript.replace("\n","<br>");
 
+        if (results[i].isFinal) {
+
+            finalTranscript += transcript;
+        } else {
+            interTranscript += transcript;
+        }
+
+            }
+            return{finalTranscript, interTranscript}
 }
 
 
 
 function stopConverting(){
+    if (recognition) {
+        recognition.stop();
+    } 
 
 }
